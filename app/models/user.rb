3e7eba2cc9d0f42defe92027bd8113b55504516b
kenshimokak <rezaei6507@gmail.com>
2018-02-10 22:41:34 +0330

    Modify user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ApplicationRecord
 	has_many :comments
+
+	before_save :dowancase_fields
+
+	validates :username, presence: true,
+			  uniqueness: { case_sensitive: false },
+			  length: { minimum: 4, maximum: 25 }
+
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: { maximum: 105 },
+			  uniqueness: { case_sensitive: false },
+			  format: { with: VALID_EMAIL_REGEX }
+
+	private 
+		def dowancase_fields
+			self.username.downcase!
+		end
+
 end
