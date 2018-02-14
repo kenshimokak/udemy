d8838b5f208394a6045b4f46af8542ca56ff8849
kenshimokak <rezaei6507@gmail.com>
2018-02-14 19:46:29 +0330

    Update all view with categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,64 @@
 class CategoriesController < ApplicationController
-	has_many :article_categories
+	before_action :set_category, only: [:show, :edit, :update, :destroy]
+	before_action :require_admin, except: [:index, :show]
+
+	def index
+		@categories = Category.paginate(page: params[:page], per_page: 5)
+	end
+
+	def new
+		@category = Category.new
+	end
+
+	def create
+		@category = Category.new(category_params)
+		respond_to do |format|
+		  if @category.save
+		    format.html { redirect_to @category, notice: 'Category was successfully created.' }
+		  else
+		    format.html { render :new }
+		  end
+		end
+	end
+
+	def edit		
+	end
+
+    def update
+      respond_to do |format|
+        if @category.update(category_params)
+          format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        else
+          format.html { render :edit }
+        end
+      end
+    end
+
+	def show
+		@category_articles = Category.find(params[:id]).articles.paginate(page: params[:page], per_page: 5)
+	end
+
+	def destroy
+	    @category.destroy
+	    respond_to do |format|
+	      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+	    end		
+	end
+
+	private
+
+		def set_category
+			@category = Category.find(params[:id])
+		end
+
+		def category_params
+			params.require(:category).permit(:name)
+		end
+
+		def require_admin
+			if !is_admin?
+				flash[:notice] = "You Can't do this action!"
+				redirect_back fallback_location: root_path
+			end
+		end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,12 @@
 class Category < ApplicationRecord
 	has_many :article_categories
 	has_many :articles, through: :article_categories
-	validates :name, presence: true, length: { minimum: 3, maximum: 25 }
-	validates_uniqueness_of :name
+	validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
+
+	before_save :down_case
+
+	private
+		def down_case
+			self.name.downcase!
+		end
 end
